bf2336f7a69bb267f99646bc691fd492a3508fbf
Adeel Hussain <adeelh@kth.se>
2021-12-06 16:49:28 +0100

    Added file

---
+++ src/components/SpotifyAPI.js
@@ -0,0 +1,3 @@
+export const API_REDIRECTURI = "http://localhost:3000/callback/";
+export const API_CLIENTSECRET = "7ea1de0c29144f3b9f0f35e7216e97ed";
+export const API_CLIENTID = "d1e2c8099a8d4bb1a5f47c84bdfb9d44";
