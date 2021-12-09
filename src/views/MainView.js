39342c7ceec317c28f58e19c25e6c8a44d902b73
Adeel Hussain <adeelh@kth.se>
2021-12-09 14:57:27 +0100

    Minor changes
    Co-Authored-By: Dana Ismail <54773040+DanaIso@users.noreply.github.com>

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,12 +1,9 @@
 import React from "react";
-import SearchResultView from "./SearchView.js";
-import promiseNoData from "../components/promiseNoData.js";
-
 
 export default function MainView(props) {
 	console.log('MainView', props);
 	return (
-		<div>
+		<div className="main">
 		
 			<h2>Welcome {props.isLoggedIn ? "Username" : "Guest"}</h2>
 			<span>Please sign up or log in to get started</span>
