5fa95f7752279dfa23dfcd71eebd699834cee611
Adeel Hussain <adeelh@kth.se>
2021-12-14 16:06:39 +0100

    Minor Changes
    Co-Authored-By: Mehir Wolde <13528811+MehirWolde@users.noreply.github.com>
    Co-Authored-By: Dana Ismail <54773040+DanaIso@users.noreply.github.com>
    Co-Authored-By: Danielmebrahtu <79120174+Danielmebrahtu@users.noreply.github.com>

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 const firebaseApp = initializeApp({
-  apiKey: REACT_APP_FIREBASE_API,
-  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: REACT_APP_FIREBASE_APPID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENTID
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 });
 
 export const database = getFirestore();
