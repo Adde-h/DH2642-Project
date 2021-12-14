782d6b309559ff7cfa99741aee901958825751d6
Adeel Hussain <adeelh@kth.se>
2021-12-14 16:01:07 +0100

    Fixed some files for Heroku Launch
    Co-Authored-By: Mehir Wolde <13528811+MehirWolde@users.noreply.github.com>
    Co-Authored-By: Dana Ismail <54773040+DanaIso@users.noreply.github.com>
    Co-Authored-By: Danielmebrahtu <79120174+Danielmebrahtu@users.noreply.github.com>

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Initialize Cloud Firestore through Firebase
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+const firebaseApp = initializeApp({
+  apiKey: REACT_APP_FIREBASE_API,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENTID
+});
+
+export const database = getFirestore();
