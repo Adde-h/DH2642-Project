01edaba6bd27d1fb9c2bd82dfaf01534c65a06b6
Dana Ismail <dana.iso@hotmail.com>
2021-12-15 12:16:40 +0100

    Added Info about Artist
    Co-Authored-By: Adeel Hussain <60479388+Adde-h@users.noreply.github.com>
    Co-Authored-By: Mehir Wolde <13528811+MehirWolde@users.noreply.github.com>
    Co-Authored-By: Danielmebrahtu <79120174+Danielmebrahtu@users.noreply.github.com>

--- src/views/ResultView.js
+++ src/views/ResultView.js
@@ -3,11 +3,29 @@ import { Link } from 'react-router-dom';
 
 
 export default function ResultView(props) {
-	return (
-		<div className="main">		
-			<button className="btnInResView"><Link to="/callback">Back to main page</Link></button>
-			<button className="btnInResView" onClick={e => props.addArtist(props.result)}>Add Artist to your list!</button>
-			<h2>{props.result.name}</h2>
-		</div>
-	);
+
+	if (props.result.type === "artist") {
+		return (
+			<div className="main">		
+				<button className="btnInResView"><Link to="/callback">Back to main page</Link></button>
+				<button className="btnInResView" onClick={e => props.addArtist(props.result)}>Add Artist to your list!</button>
+				<h1>{props.result.name}</h1>
+				<div className="resultDivD">
+					<img className="resultImageD" src={props.result.images[0].url} alt="artist img"/>
+					<div className="resultTextD">
+						This is {props.result.name}. An artist who offers his {props.result.followers.total} unique followers a taste in the {props.result.genres[0]} genre. {props.result.name} has received a spotify ranking of {props.result.popularity} based on the artist's popularity.
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="main">
+				<button className="btnInResView"><Link to="/callback">Back to main page</Link></button>
+				<button className="btnInResView" onClick={e => props.addArtist(props.result)}>Add Artist to your list!</button>
+				<h2>{props.result.name}</h2>
+			</div>
+		);
+	}
+	
 }
