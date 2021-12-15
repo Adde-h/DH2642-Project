fe08cd08522afa3b8dfef299128695bb2bd37122
Adeel Hussain <adeelh@kth.se>
2021-12-15 12:17:16 +0100

    Update ResultPresenter.js
    Co-Authored-By: Dana Ismail <54773040+DanaIso@users.noreply.github.com>
    Co-Authored-By: Danielmebrahtu <79120174+Danielmebrahtu@users.noreply.github.com>
    Co-Authored-By: Mehir Wolde <13528811+MehirWolde@users.noreply.github.com>

--- src/presenters/ResultPresenter.js
+++ src/presenters/ResultPresenter.js
@@ -9,12 +9,6 @@ console.log("ResultPresenter", props.model.currentClick);
 	return (
 		<ResultView
 			result={props.model.currentClick}
-			addArtist={(item) => props.model.setArtists(item)}
-			addAlbum={(item) => props.model.setAlbums(item)}
-			addTrack={(item) => props.model.setTracks(item)}
-			remArtist={(item) => props.model.removeArtists(item)}
-			remAlbum={(item) => props.model.removeAlbums(item)}
-			remTrack={(item) => props.model.removeTracks(item)}
 			add = {(item) => props.model.add(item)}
 			remove = {(item) => props.model.remove(item)}
 		/>
